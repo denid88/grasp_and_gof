6c7429ba7dacfed7281063fead8d0732e371d09e
denid88 <denisdubov88@gmail.com>
2023-06-08 13:42:19 +0300

    [GRASP] - creator

---
+++ grasp/creator.dart
@@ -0,0 +1,51 @@
+/// Creator (творець)
+/// Проблема: Хто повинен створювати обʼєкти в системі?
+/// Вирішення: Кожен клас повинен створюватися тим класом, який його використовує
+
+/// Solution
+class Order {
+  final List<OrderItem> _orderItems;
+
+  Order({List<OrderItem>? orderItems})
+      : _orderItems = orderItems ?? [];
+
+  int getPrice () {
+    var result = 0;
+
+    for (OrderItem orderGoodItem in _orderItems) {
+      result += orderGoodItem.getPrice();
+    }
+
+    return result;
+  }
+
+  void addOrderItem(int amount, String name, int price) {
+    _orderItems.add(OrderItem.fromData(amount, name, price));
+  }
+}
+
+class OrderItem {
+  final Good _good;
+  final int _amount;
+
+  const OrderItem(this._amount, this._good);
+
+  factory OrderItem.fromData(int amount, String name, int price) {
+    final Good good = Good(name, price);
+    return OrderItem(amount, good);
+  }
+
+  int getPrice() {
+    return _amount * _good.price;
+  }
+}
+
+class Good {
+  final String _name;
+  final int _price;
+
+  const Good(this._name, this._price);
+
+  int get price => _price;
+  String get name => _name;
+}
--- main.dart
+++ main.dart
@@ -1,14 +1,19 @@
 import 'dart:io';
 
-import 'grasp/information_expert.dart';
+//import 'grasp/information_expert.dart';
+import 'grasp/creator.dart';
 
 void main() {
   print('Version Dart: ${Platform.version}');
   /// GRASP: Information Expert
-  OrderGood order = OrderGood([
-    OrderGoodItem(GoodGood(20), 2),
-    OrderGoodItem(GoodGood(40), 2),
-    OrderGoodItem(GoodGood(20), 2),
-  ]);
-  print('Get price: ${order.getPrice()}');
+  // OrderGood order = OrderGood([
+  //   OrderGoodItem(GoodGood(20), 2),
+  //   OrderGoodItem(GoodGood(40), 2),
+  //   OrderGoodItem(GoodGood(20), 2),
+  // ]);
+  // print('Get price: ${order.getPrice()}');
+  /// GRASP: Creator
+  // final Order order = Order();
+  // order.addOrderItem(2, "Sofa", 2000);
+  // print('Get price: ${order.getPrice()}');
 }
